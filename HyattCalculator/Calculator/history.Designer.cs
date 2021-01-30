fa0b4a7666e52ebe4406edae8536c7601f52c6f2
Juvon Hyatt <juvonhyatt@yahoo.com>
2021-01-30 17:28:29 -0500

    Add files via upload

---
+++ HyattCalculator/Calculator/history.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace Calculator
+{
+    partial class History
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.history_box = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkOrange;
+            this.label1.Location = new System.Drawing.Point(129, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "History";
+            // 
+            // history_box
+            // 
+            this.history_box.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.history_box.ForeColor = System.Drawing.Color.White;
+            this.history_box.Location = new System.Drawing.Point(12, 41);
+            this.history_box.Multiline = true;
+            this.history_box.Name = "history_box";
+            this.history_box.Size = new System.Drawing.Size(319, 397);
+            this.history_box.TabIndex = 0;
+            this.history_box.Text = " ";
+            this.history_box.TextChanged += new System.EventHandler(this.history_box_TextChanged);
+            // 
+            // History
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(343, 450);
+            this.Controls.Add(this.history_box);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(361, 497);
+            this.MinimumSize = new System.Drawing.Size(361, 497);
+            this.Name = "History";
+            this.Text = "history";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox history_box;
+    }
+}
